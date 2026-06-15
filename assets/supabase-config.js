f160e840a151f231239315bd087fb2f1a238777b
ben342955678-dotcom <ben342955678@gmail.com>
2026-06-15 16:28:41 +0800

    Update supabase-config.js

--- assets/supabase-config.js
+++ assets/supabase-config.js
@@ -1,5 +1,5 @@
 window.DKSY_SUPABASE_CONFIG = {
   mode: "supabase",
   url: "https://dqezctfmocnkjcersnfo.supabase.co",
-  anonKey: "sb_publishable_xxxxxxxxxxxxxxxxxxxxxxxxx"
+  anonKey: "sb_publishable_F6UdxABEihwon2BUax65LA_T_N3ZyxH"
 };
